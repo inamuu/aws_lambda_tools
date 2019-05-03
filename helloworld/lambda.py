cc4a1ec40043b4ad08ad3f0146c70141fd2a62af
inamuu <8310973+inamuu@users.noreply.github.com>
2019-05-03 21:31:43 +0900

    :+1: Slack通知するまで

--- helloworld/lambda.py
+++ helloworld/lambda.py
@@ -1,2 +1,16 @@
-def main(event, context):
-    print('hello world')
+# coding: utf-8
+
+import json
+import requests
+import os
+
+SLACK_CHANNEL = os.environ['SLACK_CHANNEL']
+SLACK_POSTURL = os.environ['SLACK_POSTURL']
+
+def lambda_handler(event, context):
+    slack_message = {
+        'channel': SLACK_CHANNEL,
+        'icon_emoji': ":robot_face:",
+        'text': 'helloworld',
+    }
+    requests.post(SLACK_POSTURL, data=json.dumps(slack_message))
