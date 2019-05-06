f7569c6a1a6c61fa49496df862304a10a63b1937
inamuu <8310973+inamuu@users.noreply.github.com>
2019-05-06 22:47:31 +0900

    :+1: 通知内容を少しリッチにする

--- todoist_notify/lambda.py
+++ todoist_notify/lambda.py
@@ -26,22 +26,32 @@ def tasklist(name):
 
     items = todoistapi.state['items']
     slackmessage = []
-    title = "*[定期通知] プロジェクト " + name + " のタスクリスト*\n"
     for name in items:
         if name['checked'] == 0:
             if name['project_id'] == tasks_project_id:
                 taskcontent = '- ' + name['content']
                 slackmessage.append(taskcontent)
-    message = title + '\n'.join(slackmessage)
+    message = '\n'.join(slackmessage)
     return message
 
 def lambda_handler(event, context):
     name = os.environ['TODOIST_PJT']
     msg = tasklist(name)
+    title = "*[定期通知] プロジェクト " + name + " のタスクリスト*\n"
     slack_message = {
         'channel': SLACK_CHANNEL,
         'icon_emoji': ":todoist:",
-        'text': msg,
+        'text': title,
+        "attachments": [
+            {
+                "color": "#36a64f",
+                "fields": [
+                    {
+                        "value": msg,
+                    },
+                ],
+            }
+        ]
     }
     requests.post(SLACK_POSTURL, data=json.dumps(slack_message))
 
