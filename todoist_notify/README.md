a8249968164e594e68de9938a46733fe3d96c8d2
inamuu <8310973+inamuu@users.noreply.github.com>
2019-05-06 20:35:12 +0900

    :+1: lambdaを使ってSlack通知できるところまで

--- todoist_notify/README.md
+++ todoist_notify/README.md
@@ -1,3 +1,4 @@
+# todoist_notify with AWS lambda
 # Set environment using envchain.
 
 See https://techlife.cookpad.com/entry/envchain
@@ -16,7 +17,7 @@ $ envchain --set slack SLACK_CHANNEL
 Run python-lambda-local
 
 ```sh
-$ envchain slack python-lambda-local -f lambda_handler lambda.py event.json
+$ TODOISTAPITOKEN=XXXXX envchain slack python-lambda-local -f lambda_handler lambda.py event.json
 [root - INFO - 2019-05-03 21:36:15,339] Event: {}
 [root - INFO - 2019-05-03 21:36:15,340] START RequestId: 0f9e73be-e070-4011-bec7-c30adff2dbc6 Version:
 [root - INFO - 2019-05-03 21:36:15,788] END RequestId: 0f9e73be-e070-4011-bec7-c30adff2dbc6
--- todoist_notify/lambda.py
+++ todoist_notify/lambda.py
@@ -3,15 +3,53 @@
 import json
 import requests
 import os
+import sys
+from todoist.api import TodoistAPI
 
 SLACK_CHANNEL = os.environ['SLACK_CHANNEL']
 SLACK_POSTURL = os.environ['SLACK_POSTURL']
 NOTIFY_USER = os.environ['NOTIFY_USER']
+todoistapi = TodoistAPI(os.environ['TODOISTAPITOKEN'])
+
+def tasklist(name):
+    list = todoistapi.state['projects']
+    for projects_id in list:
+        #if projects_id['name'] == args.tasks:
+        if projects_id['name'] == name:
+            tasks_project_id = projects_id['id']
+            break
+
+    try:
+        tasks_project_id
+    except NameError:
+        print("プロジェクト名が正しくありません。プロジェクト名を正しく入力してください。")
+        sys.exit()
+
+    items = todoistapi.state['items']
+    slackmessage = []
+    print('### タスク一覧(id, 内容)')
+    for name in items:
+        if name['project_id'] == tasks_project_id:
+            taskid = name['id']
+            taskcontent = name['content']
+            message = (str(
+                '- '
+                + taskcontent))
+            slackmessage.append(taskcontent)
+    message = '\n'.join(slackmessage)
+    return message
 
 def lambda_handler(event, context):
+    name = os.environ['TODOIST_PJT']
+    msg = tasklist(name)
     slack_message = {
         'channel': SLACK_CHANNEL,
         'icon_emoji': ":robot_face:",
-        'text': 'helloworld',
+        'text': msg,
     }
     requests.post(SLACK_POSTURL, data=json.dumps(slack_message))
+
+## for Debug
+#if __name__ == '__main__':
+#    name = os.environ['TODOIST_PJT']
+#    tasklist(name)
